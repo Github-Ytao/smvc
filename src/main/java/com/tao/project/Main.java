6003ea391e7f35f6d63f43be5d936a4d180bb941
yangtao <626917541@qq.com>
2021-11-27 13:33:00 +0800

    remove app.properties

--- src/main/java/com/tao/project/Main.java
+++ src/main/java/com/tao/project/Main.java
@@ -7,8 +7,6 @@ package com.tao.project;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("hello world!");
-        System.out.println("hello git!");
-        System.out.println("hello java!");
+
     }
 }
